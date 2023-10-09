670e7f4cf780017d42dbca6844f7a1c5dd7d32a0
Danilo Vicentin S <danilovicentindasilva@gmail.com>
2023-10-09 16:25:34 -0300

    Taildwind Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      "primary-blue": "#106EB0",
+      "primary-orange": "#EC7000",
+      "primary-gray": "#33303E",
+      "second-gray": "#4E4B59",
+      "gray-phone": "#F4F4F4",
+      "txt-gray": "7A7786",
+      "opacity-gray": "rgba(100,80,57,0.1)",
     },
   },
   plugins: [],
